e141230391333c3fcc1bed88c02809dbe32e80a6
HowardYaw <yjhhoward@gmail.com>
2021-01-24 12:22:56 +0800

    remove comment and unused

--- src/graphql/Client.js
+++ src/graphql/Client.js
@@ -1,26 +1,9 @@
-import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const errorLink = onError(({ graphqlError, networkError }) => {
-  if (graphqlError) {
-    graphqlError.map(({ message, location, path }) => {
-      console.log("GRAPHQL ERROR: ", message);
-    });
-  }
-  if (networkError) {
-    console.log("NETWORK ERROR: ", networkError.message);
-  }
-});
 // TODO: replace with prod url
-const SERVER_LINK = from([
-  errorLink,
-  new HttpLink("http://192.168.1.51:3000/graphql"),
-]);
+const uri = "http://192.168.43.160:3000/graphql";
 
-/**
- * Fix
- */
 export default new ApolloClient({
-  uri: "http://192.168.1.51:3000/graphql",
+  uri: uri.toString(),
   cache: new InMemoryCache(),
 });
--- src/graphql/query/ImageStorage.js
+++ src/graphql/query/ImageStorage.js
@@ -1,8 +1,5 @@
 import { gql } from "@apollo/client";
 
-/**
- * Fix
- */
 export const ALL_IMAGE_STOARGES = gql`
   query GetImageStorages($filter: String!) {
     getImageStorages(filter: $filter) {
@@ -15,9 +12,6 @@ export const ALL_IMAGE_STOARGES = gql`
   }
 `;
 
-/**
- * Fix
- */
 export const CERATE_NEW_IMAGE = gql`
   mutation CreateImageStorage($imageStorageInput: ImageStorageInput!) {
     createImageStorage(imageStorageInput: $imageStorageInput) {
