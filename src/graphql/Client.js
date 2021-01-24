76ffdf3025deefbaffcd27635b3b862c5eeaf910
HowardYaw <yjhhoward@gmail.com>
2021-01-24 13:44:57 +0800

    api request with auth token

--- src/graphql/Client.js
+++ src/graphql/Client.js
@@ -1,9 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink, concat } from "@apollo/client";
+import { getToken } from "../utils/AuthenticationUtils";
 
 // TODO: replace with prod url
 const uri = "http://192.168.43.160:3000/graphql";
 
+const httpLink = new HttpLink({ uri: uri.toString() });
+
+const authMiddleware = new ApolloLink(async (operation, forward) => {
+  const token = await getToken();
+  // add the authorization to the headers
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  });
+
+  return forward(operation);
+})
+
 export default new ApolloClient({
-  uri: uri.toString(),
+  link: concat(authMiddleware, httpLink),
   cache: new InMemoryCache(),
 });
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Button, Alert } from "react-native";
+import { useQuery, useMutation } from "@apollo/client";
+import { ALL_IMAGE_STOARGES, CERATE_NEW_IMAGE } from "../../graphql";
+import { signIn } from "../../utils/AuthenticationUtils";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SignIn = ({ navigation }) => {
+  // TESTING PURPOSE
+  // const testToken = "PUT_YOUR_TOKEN"
+
+  // useEffect(() => {
+  //   signIn(testToken)
+  //   // const savedToken = await AsyncStorage.getItem("AUTH_TOKEN")
+  //   // console.log(savedToken)
+  // })
+  // const { loading, error, data: imagesData } = useQuery(ALL_IMAGE_STOARGES, {
+  //   variables: { filter: "{}" },
+  // });
+
+  // const [addImage, { data: newImage, error: createImageErr }] = useMutation(
+  //   CERATE_NEW_IMAGE
+  // );
+
+  // const dumbImage = {
+  //   id: null,
+  //   imageURL: "http://www.storage/abc",
+  //   imageSize: 70,
+  //   imageType: "images/jpeg",
+  //   imageFileNm: "Image from Workshop",
+  // };
+
+  // /**
+  //  * Fix
+  //  */
+  // if (imagesData) {
+  //   console.log("imagesData :>> ");
+  // }
+
+  // if (newImage) {
+  //   console.log("newImage :>> ", newImage);
+  // }
+
+  // if (createImageErr) {
+  //   console.log("error :>> ", createImageErr);
+  // }
 
   return (
     <View>
       <Button
         title="Go To Home"
         onPress={() => {
-          navigation.navigate("Home")
+          Alert.alert("HOME")
+          // addImage({ variables: { imageStorageInput: dumbImage } });
         }}
       />
     </View>
