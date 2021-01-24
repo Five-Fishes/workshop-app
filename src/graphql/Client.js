e2b56d08c4ca972ff34fd56856e5b60bc5bf6882
LUXIANZE <luxianze98@gmail.com>
2021-01-24 11:43:44 +0800

    Fix apollo client connection and gql format

--- src/graphql/Client.js
+++ src/graphql/Client.js
@@ -5,19 +5,22 @@ const errorLink = onError(({ graphqlError, networkError }) => {
   if (graphqlError) {
     graphqlError.map(({ message, location, path }) => {
       console.log("GRAPHQL ERROR: ", message);
-    })
+    });
   }
   if (networkError) {
-    console.log("NETWORK ERROR: ", networkError.message)
+    console.log("NETWORK ERROR: ", networkError.message);
   }
-})
+});
 // TODO: replace with prod url
 const SERVER_LINK = from([
   errorLink,
-  new HttpLink("http://localhost:3000/graphql")
+  new HttpLink("http://192.168.1.51:3000/graphql"),
 ]);
 
+/**
+ * Fix
+ */
 export default new ApolloClient({
-  link: SERVER_LINK,
-  cache: new InMemoryCache()
+  uri: "http://192.168.1.51:3000/graphql",
+  cache: new InMemoryCache(),
 });
--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,30 +1,50 @@
 import React from "react";
-import { View, Button, Alert } from "react-native";
+import { View, Button, Alert, FlatList, Text } from "react-native";
 import { useQuery, useMutation } from "@apollo/client";
 import { ALL_IMAGE_STOARGES, CERATE_NEW_IMAGE } from "../../graphql";
 
 const SignIn = ({ navigation }) => {
-  const { loading, error, imagesData } = useQuery(ALL_IMAGE_STOARGES);
-  
-  const [addImage, { newImage }] = useMutation(CERATE_NEW_IMAGE, {variables: {imageStorageInput: dumbImage}});
+  const { loading, error, data: imagesData } = useQuery(ALL_IMAGE_STOARGES, {
+    variables: { filter: "{}" },
+  });
+
+  const [addImage, { data: newImage, error: createImageErr }] = useMutation(
+    CERATE_NEW_IMAGE
+  );
 
   const dumbImage = {
+    id: null,
     imageURL: "http://www.storage/abc",
     imageSize: 10,
     imageType: "images/jpeg",
-    imageFileNm: "Image from Workshop"
+    imageFileNm: "Image from Workshop",
+  };
+
+  /**
+   * Fix
+   */
+  if (imagesData) {
+    console.log("imagesData :>> ", imagesData.getImageStorages);
   }
-  
+
+  if (newImage) {
+    console.log("newImage :>> ", newImage);
+  }
+
+  if (createImageErr) {
+    console.log("error :>> ", createImageErr);
+  }
+
   return (
     <View>
       {console.log(loading, error, imagesData)}
       <Button
         title="Go To Home"
         onPress={() => {
-          addImage();
-          console.log(newImage);
+          addImage({ variables: { imageStorageInput: dumbImage } });
         }}
       />
+      {imagesData && <Text>{imagesData.getImageStorages.toString()}</Text>}
     </View>
   );
 };
